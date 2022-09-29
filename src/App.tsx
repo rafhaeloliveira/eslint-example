70f750dad0d12820dd0d8b167c40f37dc74b0398
Rafhael Francisco De Oliveira <rafhael.oliveira@ojc.com.br>
2022-09-29 14:40:25 -0300

    adiciona eslintrc.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
 
-function App() {
+const App = (): ReactElement => {
+  let x;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,8 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>    
   );
-}
+};
 
 export default App;
